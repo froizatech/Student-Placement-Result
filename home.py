178f0d0e874482a68bb2dec1ffef8d014d60c05b
froizatech <fardhan.roiza@gmail.com>
2026-03-08 15:31:27 +0700

    Update home.py
    erasing kaggle database link

--- home.py
+++ home.py
@@ -10,6 +10,3 @@ with st.container(horizontal_alignment="center"):
 st.header('Welcome to the F5 Placement Test Website', text_alignment="center")
 
 st.markdown("F5 test 2026. Please go to the Result Announcement tab to view your placement test results.", text_alignment="center")
-
-st.markdown("Data Source: https://www.kaggle.com/datasets/sonalshinde123/student-placement-dataset/data (cleaned)")
-
